7c9cacb63379b09eb11d491de40875d22016d4cc
xiaobaigou1000 <xiaobaigou2000@qq.com>
2020-03-30 21:34:06 +0800

    add shader with vertex input

--- TryVulkanAPI/shaders/build.bat
+++ TryVulkanAPI/shaders/build.bat
@@ -1,3 +1,5 @@
 C:\VulkanSDK\1.2.131.2\Bin\glslc.exe simpleTriangle.vert -o simpleTriangleVert.spv
 C:\VulkanSDK\1.2.131.2\Bin\glslc.exe simpleTriangle.frag -o simpleTriangleFrag.spv
+C:\VulkanSDK\1.2.131.2\Bin\glslc.exe triangleWithAttrib.vert -o triangleWithAttribVert.spv
+C:\VulkanSDK\1.2.131.2\Bin\glslc.exe triangleWithAttrib.frag -o triangleWithAttribFrag.spv
 pause 
