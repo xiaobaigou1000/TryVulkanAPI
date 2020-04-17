a71a12891ae576a84d4be1c326e42bb8501c31a7
xiaobaigou1000 <xiaobaigou2000@qq.com>
2020-04-17 20:11:03 +0800

    Gauss–Seidel method

--- TrySimpleCompute/Source.cpp
+++ TrySimpleCompute/Source.cpp
@@ -2,6 +2,10 @@
 #include<vulkan/vulkan.hpp>
 #include<fstream>
 #include<iomanip>
+#include<opencv2/core.hpp>
+#include<armadillo>
+#include<chrono>
+#include<thread>
 
 VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
     VkDebugUtilsMessageSeverityFlagBitsEXT           messageSeverity,
@@ -26,6 +30,10 @@ public:
     vk::DebugUtilsMessengerCreateInfoEXT debugCreateInfo;
     vk::DebugUtilsMessengerEXT debugMessenger;
 
+    inline vk::Device getDevice()const { return device; }
+    inline vk::Queue getQueue()const { return queue; }
+    inline vk::CommandPool getCommandPool()const { return commandPool; }
+
     SimpleComputeContext()
     {
         fillDebugCreateInfo();
@@ -33,7 +41,7 @@ public:
         createDebugCallBack();
         selectPhysicalDevice();
         createLogicalDevice();
-        getQueue();
+        initQueue();
         createCommandPool();
     }
 
@@ -116,7 +124,7 @@ public:
         device = physicalDevice.createDevice(deviceInfo);
     }
 
-    void getQueue()
+    void initQueue()
     {
         queue = device.getQueue(queueFamilyIndex, 0);
     }
@@ -127,7 +135,7 @@ public:
         commandPool = device.createCommandPool(commandPoolInfo);
     }
 
-    uint32_t findMemoryType(uint32_t typeFilter, vk::MemoryPropertyFlags properties)
+    uint32_t findMemoryType(uint32_t typeFilter, vk::MemoryPropertyFlags properties)const
     {
         vk::PhysicalDeviceMemoryProperties memoryProperties = physicalDevice.getMemoryProperties();
         for (uint32_t i = 0; i < memoryProperties.memoryTypeCount; ++i)
@@ -143,55 +151,121 @@ public:
     }
 };
 
-int main()
+std::tuple<vk::Buffer, vk::DeviceMemory> createHostBuffer(const SimpleComputeContext& context, vk::Device device, vk::DeviceSize size, vk::BufferUsageFlags usage)
 {
-    SimpleComputeContext context;
-    vk::Device device = context.device;
-    float input[50];
-    for (uint32_t i = 0; i < 50; i++)
-    {
-        input[i] = i;
-    }
-
-    std::cout << "input :\n";
-    for (int i = 0; i < 5; i++)
-    {
-        for (int j = 0; j < 10; j++)
-        {
-            std::cout << std::setw(5) << input[10 * i + j];
-        }
-        std::cout << '\n';
-    }
-    std::cout << "\n\n\n";
     vk::BufferCreateInfo bufferInfo(
         {},
-        sizeof(input),
+        size,
         vk::BufferUsageFlagBits::eStorageBuffer,
         vk::SharingMode::eExclusive,
         1,
         &context.queueFamilyIndex);
-    vk::Buffer inputBuffer = device.createBuffer(bufferInfo);
-    vk::MemoryRequirements inputBufferMemoryRequirement = device.getBufferMemoryRequirements(inputBuffer);
+    vk::Buffer buffer = device.createBuffer(bufferInfo);
+    vk::MemoryRequirements requirements = device.getBufferMemoryRequirements(buffer);
     vk::MemoryAllocateInfo allocInfo(
-        inputBufferMemoryRequirement.size,
+        requirements.size,
         context.findMemoryType(
-            inputBufferMemoryRequirement.memoryTypeBits,
-            vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent));
-    vk::DeviceMemory inputBufferMemory = device.allocateMemory(allocInfo);
-    device.bindBufferMemory(inputBuffer, inputBufferMemory, 0);
+            requirements.memoryTypeBits,
+            vk::MemoryPropertyFlagBits::eHostCoherent | vk::MemoryPropertyFlagBits::eHostVisible));
+    vk::DeviceMemory memory = device.allocateMemory(allocInfo);
+    device.bindBufferMemory(buffer, memory, 0);
+    return { buffer,memory };
+}
 
-    void* data = device.mapMemory(inputBufferMemory, 0, sizeof(input));
-    memcpy(data, input, sizeof(input));
-    device.unmapMemory(inputBufferMemory);
+void destroyBufferAndFreeMemory(vk::Device device, vk::Buffer buffer, vk::DeviceMemory memory)
+{
+    device.destroyBuffer(buffer);
+    device.freeMemory(memory);
+}
 
-    vk::DescriptorSetLayoutBinding uniformBinding(0, vk::DescriptorType::eStorageBuffer, 1, vk::ShaderStageFlagBits::eCompute, nullptr);
-    vk::DescriptorSetLayoutCreateInfo descriptorSetLayoutCreateInfo({}, 1, &uniformBinding);
+int main()
+{
+    SimpleComputeContext context;
+    vk::Device device = context.getDevice();
+    vk::Queue queue = context.getQueue();
+    vk::CommandPool commandPool = context.getCommandPool();
+
+    arma::Mat<float> inputMat(2, 2);
+    inputMat(0, 0) = 16;
+    inputMat(0, 1) = 3;
+    inputMat(1, 0) = 7;
+    inputMat(1, 1) = -11;
+
+    std::cout << "origin matrix A :\n" << inputMat << std::endl;
+
+    float b[2];
+    b[0] = 11;
+    b[1] = 13;
+
+    std::cout << "origin vector b :\n" << b[0] << '\n' << b[1] << '\n';
+
+    float x0[2];
+    x0[0] = 1.0;
+    x0[1] = 1.0;
+
+    std::cout << "choosen vector x0 :\n" << x0[0] << '\n' << x0[1] << '\n';
+
+    arma::Mat<float> L = arma::trimatl(inputMat);
+    for (int i = 0; i < L.n_rows; i++)
+        L(i, i) = 0;
+    arma::Mat<float> U = arma::trimatu(inputMat);
+    for (int i = 0; i < U.n_rows; i++)
+        U(i, i) = 0;
+    arma::Mat<float> D = arma::diagmat(inputMat);
+    arma::Mat<float> DLInv = (D + L).i();
+
+    vk::Buffer dlInvBuffer;
+    vk::DeviceMemory dlInvMemory;
+    std::tie(dlInvBuffer, dlInvMemory) = createHostBuffer(
+        context, device,
+        DLInv.n_elem * sizeof(float),
+        vk::BufferUsageFlagBits::eStorageBuffer);
+
+    void* data = device.mapMemory(dlInvMemory, 0, VK_WHOLE_SIZE);
+    memcpy(data, DLInv.memptr(), DLInv.n_elem * sizeof(float));
+    device.unmapMemory(dlInvMemory);
+
+    vk::Buffer uMat;
+    vk::DeviceMemory uMatMemory;
+    std::tie(uMat, uMatMemory) = createHostBuffer(
+        context, device,
+        U.n_elem * sizeof(float),
+        vk::BufferUsageFlagBits::eStorageBuffer);
+    data = device.mapMemory(uMatMemory, 0, VK_WHOLE_SIZE);
+    memcpy(data, U.memptr(), U.n_elem * sizeof(float));
+    device.unmapMemory(uMatMemory);
+
+    vk::Buffer xkab;
+    vk::DeviceMemory xkabMemory;
+    std::tie(xkab, xkabMemory) = createHostBuffer(
+        context, device,
+        sizeof(x0) + sizeof(b),
+        vk::BufferUsageFlagBits::eStorageBuffer);
+    data = device.mapMemory(xkabMemory, 0, VK_WHOLE_SIZE);
+    void* bPtr = (float*)data + sizeof(x0) / sizeof(float);
+    memcpy(data, x0, sizeof(x0));
+    memcpy(bPtr, b, sizeof(b));
+    device.unmapMemory(xkabMemory);
+
+    vk::Buffer resultBuffer;
+    vk::DeviceMemory resultBuffermemory;
+    std::tie(resultBuffer, resultBuffermemory) = createHostBuffer(
+        context, device,
+        sizeof(x0),
+        vk::BufferUsageFlagBits::eStorageBuffer);
+
+    vk::DescriptorSetLayoutBinding dlInvBinding(0, vk::DescriptorType::eStorageBuffer, 1, vk::ShaderStageFlagBits::eCompute, nullptr);
+    vk::DescriptorSetLayoutBinding utMatBinding(1, vk::DescriptorType::eStorageBuffer, 1, vk::ShaderStageFlagBits::eCompute);
+    vk::DescriptorSetLayoutBinding xkabBinding(2, vk::DescriptorType::eStorageBuffer, 1, vk::ShaderStageFlagBits::eCompute);
+    vk::DescriptorSetLayoutBinding resultBinding(3, vk::DescriptorType::eStorageBuffer, 1, vk::ShaderStageFlagBits::eCompute);
+    std::vector<vk::DescriptorSetLayoutBinding> uniformBindings{ dlInvBinding,utMatBinding,xkabBinding,resultBinding };
+    vk::DescriptorSetLayoutCreateInfo descriptorSetLayoutCreateInfo({}, uniformBindings.size(), uniformBindings.data());
     vk::DescriptorSetLayout descriptorSetLayout = device.createDescriptorSetLayout(descriptorSetLayoutCreateInfo);
     vk::PipelineLayoutCreateInfo pipelineLayoutCreateInfo({}, 1, &descriptorSetLayout, 0, nullptr);
     vk::PipelineLayout pipelineLayout = device.createPipelineLayout(pipelineLayoutCreateInfo);
 
     //load compute shader stage
-    std::ifstream file("./comp.spv", std::ios::binary);
+    std::ifstream file("./Gauss-Seidel.spv", std::ios::binary);
     std::vector<char> fileStr{ std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>() };
     file.close();
     vk::ShaderModuleCreateInfo createInfo({}, fileStr.size(), reinterpret_cast<const uint32_t*>(fileStr.data()));
@@ -203,55 +277,100 @@ int main()
     device.destroyShaderModule(computeShaderModule);
 
     //create descriptor pool and update descriptor set
-    vk::DescriptorPoolSize poolSize(vk::DescriptorType::eStorageBuffer, 1);
+    vk::DescriptorPoolSize poolSize(vk::DescriptorType::eStorageBuffer, 4);
     vk::DescriptorPoolCreateInfo descriptorPoolInfo({}, 1, 1, &poolSize);
     vk::DescriptorPool descriptorPool = device.createDescriptorPool(descriptorPoolInfo);
     vk::DescriptorSetAllocateInfo descriptorSetAllocInfo(descriptorPool, 1, &descriptorSetLayout);
     vk::DescriptorSet descriptorSet = device.allocateDescriptorSets(descriptorSetAllocInfo).front();
-    vk::DescriptorBufferInfo descriptorBufferInfo(inputBuffer, 0, sizeof(input));
-    vk::WriteDescriptorSet writeDescriptorSet(
+
+    vk::DescriptorBufferInfo dlInvDescriptorInfo(dlInvBuffer, 0, VK_WHOLE_SIZE);
+    vk::DescriptorBufferInfo uMatDescriptorInfo(uMat, 0, VK_WHOLE_SIZE);
+    vk::DescriptorBufferInfo xkabDescriptorInfo(xkab, 0, VK_WHOLE_SIZE);
+    vk::DescriptorBufferInfo resultDescriptorInfo(resultBuffer, 0, VK_WHOLE_SIZE);
+    vk::WriteDescriptorSet dlInvWrite(
         descriptorSet,
         0,
         0,
         1,
         vk::DescriptorType::eStorageBuffer,
         nullptr,
-        &descriptorBufferInfo,
+        &dlInvDescriptorInfo,
         nullptr);
-    device.updateDescriptorSets(writeDescriptorSet, {});
 
-    vk::CommandBufferAllocateInfo commandInfo(context.commandPool, vk::CommandBufferLevel::ePrimary, 1);
+    vk::WriteDescriptorSet uMatWrite(
+        descriptorSet,
+        1,
+        0,
+        1,
+        vk::DescriptorType::eStorageBuffer,
+        nullptr,
+        &uMatDescriptorInfo,
+        nullptr);
+
+    vk::WriteDescriptorSet xkabWrite(
+        descriptorSet,
+        2,
+        0,
+        1,
+        vk::DescriptorType::eStorageBuffer,
+        nullptr,
+        &xkabDescriptorInfo,
+        nullptr);
+
+    vk::WriteDescriptorSet resultWrite(
+        descriptorSet,
+        3,
+        0,
+        1,
+        vk::DescriptorType::eStorageBuffer,
+        nullptr,
+        &resultDescriptorInfo,
+        nullptr);
+
+    std::vector<vk::WriteDescriptorSet> descriptorWrites{ dlInvWrite,uMatWrite,xkabWrite,resultWrite };
+
+    device.updateDescriptorSets(descriptorWrites, {});
+
+    vk::CommandBufferAllocateInfo commandInfo(commandPool, vk::CommandBufferLevel::ePrimary, 1);
     vk::CommandBuffer command = device.allocateCommandBuffers(commandInfo).front();
     vk::CommandBufferBeginInfo commandBeginInfo({}, nullptr);
     command.begin(commandBeginInfo);
     command.bindPipeline(vk::PipelineBindPoint::eCompute, computePipeline);
     command.bindDescriptorSets(vk::PipelineBindPoint::eCompute, pipelineLayout, 0, descriptorSet, {});
-    command.dispatch(50, 1, 1);
+    command.dispatch(sizeof(b) / sizeof(float), 1, 1);
     command.end();
 
+    vk::Fence fence = device.createFence({});
+
     vk::SubmitInfo submitInfo(0, nullptr, nullptr, 1, &command, 0, nullptr);
-    context.queue.submit(submitInfo, {});
-    context.queue.waitIdle();
 
-    void* result = device.mapMemory(inputBufferMemory, 0, sizeof(input));
-    float output[50];
-    memcpy(output, result, sizeof(input));
-
-    std::cout << "output :\n";
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 5 * sizeof(x0) / sizeof(float); i++)
     {
-        for (int j = 0; j < 10; j++)
-        {
-            std::cout << std::setw(5) << output[10 * i + j];
-        }
-        std::cout << '\n';
+        queue.submit(submitInfo, fence);
+        device.waitForFences(fence, VK_TRUE, 0xFFFFFFFF);
+        device.resetFences(fence);
+
+        void* resultPtr = device.mapMemory(resultBuffermemory, 0, VK_WHOLE_SIZE);
+        void* xkPtr = device.mapMemory(xkabMemory, 0, VK_WHOLE_SIZE);
+        memcpy(xkPtr, resultPtr, sizeof(b));
+        device.unmapMemory(resultBuffermemory);
+        device.unmapMemory(xkabMemory);
     }
+    queue.waitIdle();
+    void* result = device.mapMemory(resultBuffermemory, 0, VK_WHOLE_SIZE);
+    float trueResult[2];
+    memcpy(trueResult, result, sizeof(b));
+
+    std::cout << "result vector :\n" << trueResult[0] << '\n' << trueResult[1] << '\n';
 
     //clean up
+    destroyBufferAndFreeMemory(device, dlInvBuffer, dlInvMemory);
+    destroyBufferAndFreeMemory(device, uMat, uMatMemory);
+    destroyBufferAndFreeMemory(device, xkab, xkabMemory);
+    destroyBufferAndFreeMemory(device, resultBuffer, resultBuffermemory);
+    device.destroyFence(fence);
     device.destroyPipeline(computePipeline);
     device.destroyDescriptorPool(descriptorPool);
     device.destroyPipelineLayout(pipelineLayout);
     device.destroyDescriptorSetLayout(descriptorSetLayout);
-    device.destroyBuffer(inputBuffer);
-    device.freeMemory(inputBufferMemory);
 }
